a852c8596a451d03b4205a0776cdaba042e3f472
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-09-20 16:38:19 -0700

    update header file

--- main.h
+++ main.h
@@ -1,15 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define MAX_BUFF_SIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-char *_strtok(char *str, const char *delim);
-int _strlen(char *str);
+extern char **environ;
+void _prompt(void);
+int exec(char *cmd);
 
 #endif
