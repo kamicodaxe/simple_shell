c39d10d51d24dd7ebd40827e31346bce927b5636
Loïc Kami wsl <lkami@bogital.com>
2023-09-20 11:36:37 +0200

    - add string_helpers.c and updated shell.h

--- shell.h
+++ shell.h
@@ -7,8 +7,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+extern char **environ;
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 ssize_t _prompt(void);
 char *_strtok(char *str, const char *delim);
+char *_strchr(const char *str, int character);
+void trimLeft(char *str);
 
 #endif
