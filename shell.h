718952c3b9f0662535b78e74023066e3acab9d02
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-09-20 19:20:13 -0700

    update header file

--- shell.h
+++ shell.h
@@ -11,9 +11,10 @@
 
 extern char **environ;
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, size_t n);
 char *_strtok(char *str, const char *delim);
 char *_strchr(const char *str, int character);
 void trimLeft(char *str);
+char *_getenv(const char *envname);
 
 #endif
