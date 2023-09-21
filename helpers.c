b1f62c53588ad272d4f7288f1eb056e2aff898d1
Loïc Kami wsl <lkami@bogital.com>
2023-09-21 22:42:31 +0200

    Add _atoi to helpers.c

--- helpers.c
+++ helpers.c
@@ -35,3 +35,27 @@ void printenv(char **env)
 		i++;
 	}
 }
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int number = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			number = number * 10 + (*s - '0');
+
+		else if (number > 0)
+			break;
+	} while (*s++);
+
+	return (number * sign);
+}
