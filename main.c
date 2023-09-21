486e0f260e62d8843e4b4eb176449432e8c0e731
Loïc Kami wsl <lkami@bogital.com>
2023-09-21 03:00:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,42 +6,110 @@
 #include <sys/wait.h>
 
 /**
- * execmd - Executes line string provided
+ * argcc - argc compute
  * @bytes_read: number of bytes read by getline
  * @line: the string to be executed
+ *
+ * Return: int argc
+ *
  */
-void execmd(size_t bytes_read, char *line)
+int argcc(size_t bytes_read, char *line)
 {
-	char *argv[100];
-	int argc = 0;
 	char *token;
+	char *line_copy;
+	int argc = 0;
+
+	/* Remove leading white spaces */
+	trimLeft(line);
+
+	if (line == NULL || *line == '\0' || *line == '\t')
+	{
+		perror("empty line");
+		return (-1);
+	}
 
 	/* Check if the last char is \n */
 	if (bytes_read > 0 && line[bytes_read - 1] == '\n')
 		line[bytes_read - 1] = '\0'; /* line should be null terminated */
 
-	/* Remove leading white spaces */
-	trimLeft(line);
+	line_copy = malloc(_strlen(line) + 1);
+	if (line_copy == NULL)
+	{
+		perror("line_copy malloc error");
+		return (-1);
+	}
+
+	_strcpy(line_copy, line);
+
+	token = _strtok(line_copy, " ");
+	if (token != NULL)
+	{
+		argc++;
+		token = _strtok(NULL, " ");
+	}
+
+	free(line_copy);
+
+	return (argc);
+}
+
+/**
+ * lineparser - parse the line of command entered by user
+ * @bytes_read: number of bytes read by getline
+ * @line: the string to be executed
+ * @argc: Pointer to argc
+ * Return: double pointer
+ */
+char **lineparser(size_t bytes_read, char *line, int *argc)
+{
+	char **argv;
+	char *token;
+	int i = 0;
+
+	*argc = argcc(bytes_read, line);
+	if (*argc == -1)
+	{
+		perror("argc error");
+		return (NULL);
+	}
+
+	argv = malloc(sizeof(char *) * (*argc + 1));
+	if (argv == NULL)
+	{
+		perror("argv malloc");
+		return (NULL);
+	}
 
 	token = _strtok(line, " ");
 	if (token == NULL)
-		return;
-
-	argv[argc++] = token;
-	/* Parse and add arguments */
-	while ((token = _strtok(NULL, " ")) != NULL)
 	{
-		argv[argc++] = token;
-		if (argc >= 99)
-			return; /* Reached the limit of 100 total arguments */
+		perror("empty token");
+		return (NULL);
 	}
 
-	argv[argc] = NULL; /* Ensure the argument list is null-terminated */
-	if (line == NULL || *line == '\0' || *line == '\t')
+	while (token != NULL)
 	{
-		exit(EXIT_SUCCESS);
-		return;
+		argv[i] = malloc(_strlen(token) + 1);
+		if (argv[i] == NULL)
+		{
+			perror("argv malloc");
+			return (NULL);
+		}
+		_strcpy(argv[i], token);
+		i++;
+		token = _strtok(NULL, " ");
 	}
+	argv[i] = NULL;
+
+	return (argv);
+}
+
+/**
+ * execmd - Executes argv array in execve
+ * @argv: double pointer to the arguments for execve
+ */
+void execmd(char **argv)
+{
 
 	if ((execve(argv[0], argv, environ)) == -1)
 	{
@@ -62,14 +130,14 @@ int main(void)
 	ssize_t bytes_read;
 	pid_t pid;
 	int status;
+	char **argv;
+	int argc = 0;
 
 	while ((bytes_read = getline(&line, &len, stream)) != -1)
 	{
-		/* Creat the fork process*/
-		pid = fork();
+		pid = fork(); /* Create the fork process*/
 
-		/* Check fork error*/
-		if (pid == -1)
+		if (pid == -1) /* Check fork error*/
 		{
 			perror("./shell0");
 			exit(EXIT_FAILURE); /* Exit due to fork error */
@@ -77,16 +145,18 @@ int main(void)
 
 		if (pid == 0)
 		{
-			/* Call execmd to execute line */
-			execmd(bytes_read, line);
-			/* Do whatever you want the child process to do here */
+			argv = lineparser(bytes_read, line, &argc);
+			if (argv == NULL)
+			{
+				perror("argv");
+				exit(EXIT_FAILURE);
+			}
+			execmd(argv); /* Call execmd to execute line */
 		}
 
 		if (pid > 0)
 		{
-			/* Wait for the child process to stop */
-			wait(&status);
-			/* Do whatever you want the parent process to do here */
+			wait(&status); /* Wait for the child process to stop */
 		}
 	}
 
