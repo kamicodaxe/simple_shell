aba2f921bc6f36f3d0f3d8fa9334e37066dfb002
Loïc Kami wsl <lkami@bogital.com>
2023-09-21 12:49:26 +0200

    Merged string helpers

--- string_helpers2.c
+++ string_helpers2.c
@@ -19,3 +19,56 @@ int _strcmp(char *s1, char *s2)
 
 	return (s1[i] - s2[i]);
 }
+
+/**
+ * _strncmp - A function that compares two strings
+ * @s1: first pointer parameter of type char
+ * @s2: second pointer parameter of type char
+ * @n: number of characters to compare
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strncmp(char *s1, char *s2, size_t n)
+{
+	while (*s1 && n > 0)
+	{
+		if (*s1 != *s2)
+		{
+			break;
+		}
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+
+	/* return ascii difference after converting *char to unsigned *char */
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+/**
+ * _strdup - A function that duplicates
+ * a string
+ * @s: original string to duplicate
+ * Return: pointer to the value of the duplicate string
+ */
+char *_strdup(char *s)
+{
+	char *d;
+	int len;
+
+	if (s == NULL)
+		return (NULL);
+	len = _strlen(s);
+	d = (char *)malloc(len + 1); /* allocate memory */
+
+	if (d == NULL)
+	{
+		perror("_strdup malloc:");
+		exit(EXIT_FAILURE);
+	}
+	_strcpy(d, s);
+	return (d);
+}
