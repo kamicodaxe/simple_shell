4b8de4508901e9427cf1dd8b78eb774173ad9d6c
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-09-22 01:25:19 -0700

    trim find_path below 40 lines

--- find_path.c
+++ find_path.c
@@ -1,6 +1,4 @@
 #include "shell.h"
-#include <stdlib.h>
-
 /**
  * find_path - A function that finds the full path of an executable
  * @cmd: The name of the executable file
@@ -12,42 +10,31 @@ char *find_path(char *cmd)
 {
 	char *p;
 	char f_path[MAX_BUF_SIZE];
+	int i;
 
 	if (cmd == NULL)
 		return (NULL); /* Invalid cmd parameter */
 
 	if (*cmd == '/' || *cmd == '.')
 		return (cmd);
-
 	p = _getenv("PATH");
 
 	if (p == NULL)
-	{
 		return (NULL);
-	}
-
 	while (*p)
 	{
-		int i = 0;
-
-		/* Copy characters from PATH to f_path until a ':' or '\0' is encountered */
+		i = 0;
 		while (*p && *p != ':')
 			f_path[i++] = *p++;
 
-		/* Null-terminate the path */
 		f_path[i] = '\0';
-
-		/* Append the command name to the path */
 		_strcat(f_path, "/");
 		_strcat(f_path, cmd);
 
-		/* Check if the path/filename is executable */
 		if (access(f_path, X_OK) == 0)
 			return (_strdup(f_path)); /* Found the executable */
-
 		if (*p)
 			p++; /* Skip the ':' if not at the end of PATH */
 	}
-
 	return (NULL); /* Executable not found in any directory */
 }
